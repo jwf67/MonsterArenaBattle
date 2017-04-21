ea3ccb9cd55cf33ca50069b711b7503efb7f9717
jwf67 <jwf67@case.edu>
2017-04-21 00:12:52 -0400

    Changed some scripts around, started adding implementation/comments for next steps

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Monster
+{
+    //Properties of a Monster
+    String name;
+    int attack;
+    int defense;
+    int hp;
+    int currHP;
+    int number;
+
+    public Monster(String name, int attack, int defense, int hp, int number)
+    {
+        this.name = name;
+        this.attack = attack;
+        this.defense = defense;
+        this.hp = hp;
+        currHP = hp;
+        this.number = number;
+    }
+}
+
+public class AccessMonsters
+{
+    private static Monster[] monsters = new Monster[5];
+
+    Monster a = new Monster("", 5, 5, 5, 1);
+    Monster b = new Monster("", 7, 7, 7, 2);
+    Monster c = new Monster("", 9, 9, 9, 3);
+    Monster d = new Monster("", 1, 1, 1, 4);
+    Monster e = new Monster("", 5, 3, 9, 5);
+
+    //add all the monsters to the pool of monsters (no back end yet)
+    public void AddMonster()
+    {
+        monsters[0] = a;
+        monsters[1] = b;
+        monsters[2] = c;
+        monsters[3] = d;
+        monsters[4] = e;
+    }
+
+
+    //get the monster at the specified index
+    public static Monster GetMonster(int i)
+    {
+        return monsters[i];
+    }
+}
---
+++ Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MonsterScript : MonoBehaviour {
+
+    //each monster is identified by its index and its monster value
+    private Monster m;
+    public int index;
+
+	// Use this for initialization
+	void Start () {
+        setMonster();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+         * //check if it's the local player
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        */
+
+        //controls
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    //upload new monster from given index
+    void setMonster()
+    {
+        m = AccessMonsters.GetMonster(index);
+    }
+
+    //damage calculation
+    void dealDamage()
+    {
+
+    }
+
+    //when another object hits this object
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "attack")
+        {
+            //do calculation
+        }
+    }
+}
+
